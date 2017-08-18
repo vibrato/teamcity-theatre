0670be0347ca93ec54eb5ecd906a0c0744100daf
Alexander Moerman <amoerman@csc.com>
2017-08-18 15:48:31 +0200

    Remove JS files from Git. These are transpiled from TS anyway

--- src/TeamCityTheatre.Web/views/home/erroralert.components.js
+++
@@ -1,9 +0,0 @@
-import { createElement } from "react";
-export var ErrorAlert = function (props) {
-    return (createElement("div", { className: "alert alert-error" },
-        createElement("strong", null, "Error!"),
-        " Can you make sense of this?",
-        createElement("div", { className: "well well-lg" },
-            createElement("h3", null, "Details"),
-            createElement("pre", { dangerouslySetInnerHTML: { __html: JSON.stringify(props.error, null, 2) } }))));
-};
