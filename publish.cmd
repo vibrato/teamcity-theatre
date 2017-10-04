52a60f95ddbc374fd81abb52bdc44883d4925c2f
Alexander Moerman <amoerman@csc.com>
2017-10-04 13:51:08 +0200

    Remove pause from build scripts

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,3 @@
 @echo off
 TITLE TeamCityTheatre -- Publish
 rmdir /S /Q "./publish-output" & dotnet restore "./src/TeamCityTheatre.sln" && dotnet clean "./src/TeamCityTheatre.sln" --configuration Release --verbosity normal && cd "./src/TeamCityTheatre.Web" && yarn install && yarn run build:release && cd .. && cd .. && dotnet publish "./src/TeamCityTheatre.Web/TeamCityTheatre.Web.csproj" --configuration Release --verbosity normal --output "./../../publish-output"
-pause
